f723d37da71329a591ac7b93b3d24d2dcc761e73
Filip van Laenen <f.a.vanlaenen@ieee.org>
2022-03-15 14:52:09 +0100

    Added the Element interface

---
+++ src/main/java/net/filipvanlaenen/bltxmlepj/Element.java
@@ -0,0 +1,24 @@
+package net.filipvanlaenen.bltxmlepj;
+
+/**
+ * Top interface for all element types.
+ */
+public interface Element {
+    /**
+     * Returns a string representation of the element.
+     *
+     * @return A string representation of the element.
+     */
+    default String asString() {
+        return asString("");
+    }
+
+    /**
+     * Returns a string representation of the element with the provided indentation.
+     *
+     * @param indent
+     *            The indentation.
+     * @return A string representation of the element with the provided indentation.
+     */
+    String asString(String indent);
+}
