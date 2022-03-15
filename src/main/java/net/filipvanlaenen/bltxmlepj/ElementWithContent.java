27295b80f5c86ec7c9cc8721f1ce3ce87afea13f
Filip van Laenen <f.a.vanlaenen@ieee.org>
2022-03-15 17:07:48 +0100

    Added a class ElementWithContent

---
+++ src/main/java/net/filipvanlaenen/bltxmlepj/ElementWithContent.java
@@ -0,0 +1,36 @@
+package net.filipvanlaenen.bltxmlepj;
+
+public abstract class ElementWithContent implements Element {
+    /**
+     * The content.
+     */
+    private final String content;
+
+    protected ElementWithContent(final String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String asString(final String indent) {
+        if (content == null || content.isEmpty()) {
+            return indent + "<" + getElementName() + "/>";
+        } else {
+            return indent + "<" + getElementName() + ">" + xmlEscape(content) + "</" + getElementName() + ">";
+        }
+    }
+
+    protected abstract String getElementName();
+
+    /**
+     * Escape special XML characters in a string. The special XML characters that
+     * will be escaped are <code>&lt;</code>, <code>&gt;</code> and
+     * <code>&amp;</code>.
+     *
+     * @param string
+     *            The original text.
+     * @return The text with special XML characters escaped.
+     */
+    private String xmlEscape(final String string) {
+        return string.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    }
+}
