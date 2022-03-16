5d68e5980b30548dd0b6af0df012c1e7756adc12
Filip van Laenen <f.a.vanlaenen@ieee.org>
2022-03-16 16:09:01 +0100

    Refactored ElementWithElements to use StringBuilder in asString

--- src/main/java/net/filipvanlaenen/bltxmlepj/ElementWithElements.java
+++ src/main/java/net/filipvanlaenen/bltxmlepj/ElementWithElements.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Abstract class providing common functionality for element types containing other elements.
+ * Abstract class providing common functionality for element types containing
+ * other elements.
  */
 public abstract class ElementWithElements implements Element {
     /**
@@ -15,7 +16,8 @@ public abstract class ElementWithElements implements Element {
     /**
      * Adds an element to the list of elements.
      *
-     * @param element The element to be added.
+     * @param element
+     *            The element to be added.
      */
     protected final void addElement(final Element element) {
         elements.add(element);
@@ -23,16 +25,24 @@ public abstract class ElementWithElements implements Element {
 
     @Override
     public final String asString(final String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent);
+        sb.append("<");
+        sb.append(getElementName());
         if (elements.isEmpty()) {
-            return indent + "<" + getElementName() + "/>";
+            sb.append("/>");
         } else {
-            List<String> elementStrings = new ArrayList<String>();
+            sb.append(">\n");
             String nextIndent = "  " + indent;
             for (Element element : elements) {
-                elementStrings.add(element.asString(nextIndent));
+                sb.append(element.asString(nextIndent));
+                sb.append("\n");
             }
-            return indent + "<" + getElementName() + ">\n" + String.join("\n", elementStrings) + "\n" + indent + "</"
-                    + getElementName() + ">";
+            sb.append(indent);
+            sb.append("</");
+            sb.append(getElementName());
+            sb.append(">");
         }
+        return sb.toString();
     }
 }
